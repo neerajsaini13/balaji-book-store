2c6a719c315ef233644830741ca9fb29519b4c92
Neeraj Saini <neerajkumartawanr32@gmail.com>
2024-07-21 11:47:12 +0530

    balaji book store project

--- routes/favouriteRouter.js
+++ routes/favouriteRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { checkUserAuth } = require("../middlewares/userAuth");
-const wrapAsync = require("../utils/wrapAsync");
+const { wrapAsync } = require("../utils/wrapAsync.js")
 const User = require("../models/User");
 
 
